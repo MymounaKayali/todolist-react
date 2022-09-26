aeac134defc2f1bb6324f7d818bd9c57d946d9cc
MymounaKayali <eng.mymouna.97@gmail.com>
2022-09-26 10:58:37 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React , {useState} from 'react';
+import './App.css';
+
+function App() {
+
+  const [newItem , setNewItem] = useState("")
+  const [items, setItems] = useState([])
+  
+  function addItem()
+{
+  if (!newItem)
+  {
+    alert("Enter an item to add")
+    return;
+  }
+  const item =
+  {
+    id: Math.floor(Math.random() * 1000),
+    value: newItem
+  }
+
+  setItems(oldList =>[...oldList, item])
+  setNewItem("")
+}
+
+function deleteItem(id)
+  {
+    const newArray = items.filter(item => item.id !== id)
+    setItems(newArray)
+    setNewItem("")  
+  } 
+
+  return (
+    <div className="App">
+    <h1>ToDo List App</h1>
+    <input type="text" placeholder='Add an item ...' value={newItem}
+    onChange = {e => setNewItem(e.target.value)}></input>
+    <button onClick={() => addItem()}>Add</button>
+
+    <ul>
+      {items.map(item => {return (<li key={item.id}>
+      {item.value} <button onClick={() =>deleteItem(item.id)}> X </button> </li>)})}
+    </ul> 
+    </div>
+  );
+}
+
+export default App;
